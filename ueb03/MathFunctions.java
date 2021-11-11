47cb8fba622a3ac457dc790d106d9dca2c06a7a6
Katharina Krier <katharina.krier@web.de>
2021-11-11 09:54:32 +0100

    Teilersumme

--- ueb03/MathFunctions.java
+++ ueb03/MathFunctions.java
@@ -1,4 +1,4 @@
-
+import java.util.ArrayList;
 /**
  * Beschreiben Sie hier die Klasse MathFunctions.
  * 
@@ -7,27 +7,40 @@
  */
 public class MathFunctions
 {
-    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
-    private int x;
-
-    /**
-     * Konstruktor für Objekte der Klasse MathFunctions
-     */
-    public MathFunctions()
-    {
-        // Instanzvariable initialisieren
-        x = 0;
-    }
-
-    /**
-     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
-     * 
-     * @param  y    ein Beispielparameter für eine Methode
-     * @return        die Summe aus x und y
-     */
-    public int beispielMethode(int y)
-    {
-        // tragen Sie hier den Code ein
-        return x + y;
-    }
+   private static final long KLEINSTER_TEILER = 1;
+   private static final long START_WERT_SUMME = 0;
+   
+   static public long berechneTeilersumme (long zahl){
+       long teilersumme   = START_WERT_SUMME;
+       long teiler        = KLEINSTER_TEILER;
+       long spiegelteiler = zahl;
+       long haelfteZahl;
+       ArrayList<Long> vorgekommeneSpiegelteiler;
+       if(zahl % 2 == 0){
+           haelfteZahl = zahl / 2;    
+           vorgekommeneSpiegelteiler = new ArrayList<Long>();
+       }
+       else{
+           haelfteZahl = (zahl + 1) / 2;
+           vorgekommeneSpiegelteiler = new ArrayList<Long>();
+       }
+       
+      while(!pruefeObInLIiste(vorgekommeneSpiegelteiler , teiler)){
+           if(zahl % teiler == 0){
+               spiegelteiler   = zahl / teiler;
+               teilersumme    += teiler;
+               teilersumme    += spiegelteiler;
+               vorgekommeneSpiegelteiler.add(spiegelteiler);
+               
+           }
+           teiler++;
+       }
+       
+       return teilersumme;
+   }
+   
+   public static boolean pruefeObInLIiste(ArrayList<Long> liste , Long zahlZumPruefen ){
+       boolean contains = liste.contains(zahlZumPruefen);
+       return contains;
+   }
 }
