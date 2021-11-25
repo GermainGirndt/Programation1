12b77a0fac3eca5be52058c74f349d6f778703a2
Katharina Krier <katharina.krier@web.de>
2021-11-25 08:57:19 +0100

    Ordner Angelegt

---
+++ ueb05/Artikel.java
@@ -0,0 +1,116 @@
+import java.util.regex.Pattern;
+
+/**
+* Die Klasse Artikel ist für eine einfache Bestandsprüfung
+*
+* @author Girndt & Krier
+* @version 1.2
+*/
+public class Artikel
+{
+    private              int        artikelNr;
+    private              String     art;
+    private              int        bestand;
+    
+    private static final int        MINIMUM_BESTAND = 0;
+
+    /**
+    * Konstruktor für Artikel mit bekannten Bestand
+    * @param artikelNr ist die ArtikelNr
+    * @param art ist die Artikelart
+    * @param bestand steht für den initalen Bestand
+    */
+    public Artikel(int artikelNr, String art, int bestand)
+    {
+        Validierung.validiereArtikelNr(artikelNr);
+        Validierung.validiereArtikelArt(art);
+        Validierung.validiereBestand(bestand);
+        
+        this.artikelNr  = artikelNr;
+        this.art        = art;
+        this.bestand    = bestand;
+    }
+
+    /**
+    * Konstruktor für Artikel ohne Bestandsangabe
+    * @param artikelNr ist die ArtikelNr
+    * @param art ist die Artikelart
+    */
+    public Artikel(int artikelNr, String art)
+    {
+        this(artikelNr, art, MINIMUM_BESTAND);
+    }
+
+    /**
+    * Die Methode bucht eine übergebene Menge dem Bestand hinzu
+    * @param mengeArtikelzugang ist die Menge neuer Artikel, die dazugebucht wird
+    */
+    public void bucheZugang(int mengeArtikelzugang)
+    {
+        Validierung.validiereMengeanderung(mengeArtikelzugang);
+        
+        this.bestand += mengeArtikelzugang;
+    }
+
+    /**
+    * Die Methode bucht eine übergebene Menge dem Bestand ab
+    * @param menge ist die Menge, die abgebucht wird
+    */
+    public void bucheAbgang(int menge)
+    {
+        Validierung.validiereAbgangsMenge(this.bestand, menge);
+        
+        this.bestand -= menge;
+    }
+
+    /**
+    * Die Methode gibt das Objekt aufbereitet als String zurueck
+    * @return gibt die Attribute des Artikels als String zurueck
+    */
+    public String toString()
+    {
+        return  "ArtikeNr: "    + artikelNr     + "\n" +
+                "Art: "         + art           + "\n" +
+                "Bestand: "     + bestand;
+
+    }
+
+    /**
+    * Die Methode gibt die Artikelnummer zurueck
+    * @return die Artikelnummer
+    */
+    public int getArtikelNr()
+    {
+        return this.artikelNr;
+    }
+
+    /**
+    * Die Methode gibt die Artikelart zurueck
+    * @return die Artikelart
+    */
+    public String getArt()
+    {
+        return this.art;
+    }
+
+    /**
+    * Die Methode gibt den Bestand zurueck
+    * @return den Bestand
+    */
+    public int getBestand()
+    {
+        return this.bestand;
+    }
+
+    /**
+    * Die Methode setzt die Artikelart neu
+    * @param neue Artikelart
+    */
+    public void setArt(String art)
+    {
+        Validierung.validiereArtikelArt(art);
+        
+        this.art = art;
+    }
+
+}
---
+++ ueb05/UserInput.java
@@ -0,0 +1,63 @@
+ 
+
+import java.util.Scanner;
+
+/**
+* Die Klasse UserInput ist eine abstrakte Klasse zur Steuerung des User Standard Inputs
+*
+* @author Girndt & Krier
+* @version 1.2
+*/
+public class UserInput {
+
+    private Scanner input;
+    
+    /**
+    * Konstruktor
+    */
+    public UserInput() {
+        input = new Scanner(System.in);
+    }
+
+    /**
+    * Die Methode zeigt eine Message an und fragt erwartet einein Integerinput von dem Nutzer
+    * Außerdem sorgt die Methode dafür, dass der Scanner nicht gesperrt bleibt, indem sie "input.nextLine()" aufruft.
+    * @param message ist die anzuzeigende Nachricht 
+    * @return ist die von Nutzer eingegebene Integer 
+    */
+    public int getInt(String message) {
+        System.out.print(message);
+        int integer = input.nextInt();
+        input.nextLine();
+        return integer;
+    }
+
+    /**
+    * Die Methode zeigt eine Message an und fragt erwartet einein Stringinput von dem Nutzer
+    * Außerdem sorgt die Methode dafür, dass der Scanner nicht gesperrt bleibt, indem sie "input.nextLine()" aufruft.
+    * @param message ist die anzuzeigende Nachricht 
+    * @return ist die von Nutzer eingegebene String 
+    */
+    public String getString(String message) {
+        System.out.print(message);
+        String string = input.nextLine();
+        return string;
+    }
+
+    /**
+    * Standardisierte nextLine Methode.
+    * Dient als Schnittstelle mit dem Scanner Objekt
+    */
+    public void nextLine() {
+        input.nextLine();
+    }
+
+    /**
+    * Standardisierte next Methode.
+    * Dient als Schnittstelle mit dem Scanner Objekt
+    */
+    public void next() {
+        input.next();
+    }
+    
+}
