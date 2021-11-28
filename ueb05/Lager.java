4a307bff1bd38737e5e736d742a00a9a9cc7462f
Katharina Krier <katharina.krier@web.de>
2021-11-28 10:53:45 +0100

    Update Lager.java

--- ueb05/Lager.java
+++ ueb05/Lager.java
@@ -8,6 +8,7 @@
 public class Lager
 {
    private Artikel[] lager;
+   int anzahlArtikel = 0;
    
    public Lager(int lagerplatzanzahl){
        lager = new Artikel[lagerplatzanzahl];   
@@ -18,6 +19,48 @@ public class Lager
    }
    
    public void legeAnArtikel(Artikel artikel){
+       lager[anzahlArtikel] = artikel;    
+       if(anzahlArtikel + 1 < lager.length){
+           anzahlArtikel++;   
+       }
        
+   }
+   
+   public void entferneArtikel(int artikelNr){
+       Artikel[] kopieLager = new Artikel[lager.length];
+       for(int i = 0; i < lager.length; i++){
+           if(lager[i] != null && lager[i].getArtikelNr() == artikelNr){
+               lager = loescheArtikel(lager, i);   
+               anzahlArtikel--;
+               i--;
+           }
+       }    
+   }
+   
+   private Artikel[] loescheArtikel(Artikel[] lager, int Index){
+       Artikel[] kopie = new Artikel[lager.length];
+       for(int i = 0; i < Index; i++){
+           kopie[i] = lager[i];
+       }
+       if(Index != lager.length){
+           for(int j = Index + 1; j < lager.length; j++ ){
+               kopie[j-1] = lager[j];
+           }
+       }
+       return kopie;    
+   }
+   
+   
+   //Nur fuer Testzwecke, denke dran das zu löschen
+   public static void main(String[] strg){
+       Artikel a = new Artikel(1 , "kuh");
+       Artikel b = new Artikel(2 , "Pferd");
+       Lager   lager = new Lager();
+       lager.legeAnArtikel(a);
+       lager.legeAnArtikel(a);
+       lager.legeAnArtikel(b);
+       lager.legeAnArtikel(a);
+       lager.legeAnArtikel(b);
+       lager.entferneArtikel(2);
    }
 }
