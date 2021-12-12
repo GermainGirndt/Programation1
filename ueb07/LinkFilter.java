64cab1bea1d103c97a73d7b58ee6117590dbe2f7
Germain Girndt <germaingirndt@outlook.com>
2021-12-12 20:11:32 +0100

    Weitere Tests hinzugefuegt, Kommentare geschrieben

--- ueb07/LinkFilter.java
+++ ueb07/LinkFilter.java
@@ -19,30 +19,20 @@ public class LinkFilter {
     private int URL_REGEX_GROUP_NUMMER = 2;
     private int INHALT_REGEX_GROUP_NUMMER = 7;
 
-    // "<a\s*(\w*=".*")*\s*href="((https?:\/\/)?(www\.)?[\w()@:%.\+~#=]{1,256}\.[\w\d]{1,6}\b([-\w()@:%\+.~#?&\/=]*))"\s*(\w*=".*")*\s*>.*<\/a>";
-
-    private String regexAnchorTagStart = "<a\\s*(\\w*=\".*\")*\\s*";
-    private String regexLink = "((https?:\\/\\/)?(www\\.)?[\\w()@:%.\\+~#=]{1,256}\\.[\\w\\d]{1,6}\\b([-\\w()@:%\\+.~#?&\\/=]*))";
-    private String href = "href=\"" + regexLink + "\"";
-    private String regexNachHref = "\\s*(\\w*=\".*\")*\\s*>";
-
-    private String regexAnchorTagOeffnung = regexAnchorTagStart + href + regexNachHref;
-    private String regexLinkDescription = "(.*)";
-    private String regexAnchorTagSchluss = "<\\/a>";
-
-    private String regex = regexAnchorTagOeffnung + regexLinkDescription + regexAnchorTagSchluss;
+    private String anchorTagRegex;
 
     /**
      * Konstruktor für Objekte der Klasse LinkFilter
      */
     public LinkFilter() {
-        reader = new BufferedReader(new InputStreamReader(System.in));
+        this.reader = new BufferedReader(new InputStreamReader(System.in));
+        this.bildeRegexFuerAnchorTag();
 
     }
 
     public void pruefeHtmlZeile(String htmlLine) {
 
-        Pattern regexPattern = Pattern.compile(regex);
+        Pattern regexPattern = Pattern.compile(anchorTagRegex);
         Matcher matcher = regexPattern.matcher(htmlLine);
         boolean matchFound = matcher.find();
 
@@ -52,20 +42,22 @@ public class LinkFilter {
             String url = matcher.group(URL_REGEX_GROUP_NUMMER);
             int anzahlZeichen = url.length();
 
-            System.out.println(String.format("%s:\t%s, Anzahl Zeichen: %d", inhalt, url, anzahlZeichen));
+            System.out.println(String.format("%s:\t\t%s, Anzahl Zeichen: %d", inhalt, url, anzahlZeichen));
             gefundeneLinks++;
-        } 
+        }
     }
 
     public void leseDateiEin() {
         try {
-            String htmlLine = reader.readLine();
+            String htmlLine = this.reader.readLine();
             while (htmlLine != null) {
                 pruefeHtmlZeile(htmlLine);
 
-                htmlLine = reader.readLine();
+                htmlLine = this.reader.readLine();
                 geleseneZeilen++;
             }
+
+            System.out.println(String.format("%d Links wurden in %d Zeilen gefunden.", gefundeneLinks, geleseneZeilen));
 
         } catch (IOException e) {
             System.err.println(e);
@@ -75,5 +67,42 @@ public class LinkFilter {
     public static void main(String[] args) {
         LinkFilter r = new LinkFilter();
         r.leseDateiEin();
+    }
+
+    private void bildeRegexFuerAnchorTag() {
+
+        
+        /**
+         * folgende optionale Bedindung werden akzeptiert:
+         * beliebige Anzahl an Leertasten geben
+         * andere Attribute (es wird nicht nach Gueltigkeit geprueft, denn ungultige
+         * Attribute können gültige Anchor-Tags generieren)
+         * beliebige Anzahl an Leertasten geben [2]
+         */
+        String leerTastenMitAttributen = "\\s*(\\w*=\".*\")*\\s*";
+
+         
+        /**
+         * Der URL darf mit/ohne http(s) anfangen
+         * Darf www. haben
+         * der Second-Level-Domain und die Subdomains müssen von 1 bis 256 Zeichen
+         * enthalten (ink. einige Sonderzeichen)
+         * der Top-Level-Domain muss von 1 bis 6 Zeichen enthalten
+         * Es werden auch Subdirectories mit verschiedene Sonderzeichen akzeptiert
+         * Inspiriert von:
+         * https://blog.hubspot.com/marketing/parts-url
+         * https://stackoverflow.com/questions/3809401/what-is-a-good-regular-expression-to-match-a-url
+         */
+        String sonderzeichen = "\\w()\\-@:%.\\+~#=";
+        String regexURL = "((https?:\\/\\/)?(www\\.)?[" + sonderzeichen + "]{1,256}\\.[\\w\\d]{1,6}\\b([" + sonderzeichen + "?&\\/]*))";
+        
+         /**
+         * Der URL wird zwischen die href Attribute gestellt
+         */
+        String regexAnchorTagOeffnung = "<a" + leerTastenMitAttributen + "href=\"" + regexURL + "\"" + leerTastenMitAttributen + ">";
+        String regexURLInhalt = "(.*)";
+        String regexAnchorTagSchluss = "<\\/a>";
+
+        this.anchorTagRegex = regexAnchorTagOeffnung + regexURLInhalt + regexAnchorTagSchluss;
     }
 }
