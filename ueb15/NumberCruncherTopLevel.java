04c8ab6cd8fe904b7bab77afdb7848dc2ae7fdda
Katharina Krier <katharina.krier@web.de>
2022-05-07 10:35:52 +0200

    TopLevel

--- ueb15/NumberCruncherTopLevel.java
+++ ueb15/NumberCruncherTopLevel.java
@@ -8,6 +8,11 @@
 public class NumberCruncherTopLevel
 {
     float[] numbers;
+    Sum sum;
+    Swirl swirl;
+    Divide divide;
+    Subtract subtract;
+    Average average;
     
     
     /**
@@ -17,6 +22,11 @@ public class NumberCruncherTopLevel
     public NumberCruncherTopLevel(float[] numbers)
     {
         this.numbers = numbers;  
+        sum = new Sum();
+        swirl = new Swirl();
+        divide = new Divide();
+        subtract = new Subtract();
+        average = new Average();
     }
 
     
@@ -26,6 +36,25 @@ public class NumberCruncherTopLevel
      */
     public void crunch(String[] operations){
         
+        for(int i=0; i < operations.length; i++){
+            switch(operations[i].toLowerCase()){
+                case "sum":
+                    sum.crunch(numbers);
+                    break;
+                case "swirl":
+                    swirl.crunch(numbers);
+                    break;
+                case "divide":
+                    divide.crunch(numbers);
+                    break;
+                case "subtract":
+                    subtract.crunch(numbers);
+                    break;
+                case "average":
+                    average.crunch(numbers);
+                    break;
+            }
+        }
     }
     
     public float[] getNumbers(){
