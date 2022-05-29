b0d72f26aa08ffb4a4314d1060879be92d24ab17
Germain Girndt <germaingirndt@outlook.com>
2022-05-29 20:54:57 +0200

    Added next tasks

--- ueb17/Functions.java
+++ ueb17/Functions.java
@@ -5,15 +5,30 @@ import java.util.function.Predicate;
  * @author (Ihr Name) 
  * @version (eine Versionsnummer oder ein Datum)
  */
-public class Functions 
-{
+public class Functions {
    public static MyFunction quadrat = (x) -> {return x*x;};
+
+   public static MyFunction quadratVonGeradeZahlen = (x) -> {
+       if (ParityChecker.odd.test(x)) {
+           return Functions.quadrat.apply(x);
+       }
+
+       return -1;
+   };
   
    public static MyFunction fact = (x) -> {
        int erg = 1;
        for(int i = 1; i<=x; i++)
             erg *= i;
        return erg;
+   };
+
+   public static MyFunction factVonUngerade = (x) -> {
+
+       if (ParityChecker.even.test(x)) {
+           return Functions.fact.apply(x);
+       }
+       return 0;
    };
    
    public static MyFunction potenz = (x) -> {return (int)Math.pow(x, x+1);};
@@ -23,14 +38,13 @@ public class Functions
         int fibonacci2 = 1;
         int fibonacci  = 1;
 
-        for(int i = 0; i < x; i++){
+        for (int i = 0; i < x; i++){
         fibonacci = fibonacci1 + fibonacci2;
         fibonacci1 = fibonacci2;
         fibonacci2 = fibonacci;
         }
         return fibonacci;
        };
-       
    
    public static class NestedFact implements MyFunction{
         @Override
@@ -84,28 +98,28 @@ public class Functions
        
        Functions f = new Functions();
        Fakultaet factTopLevel = new Fakultaet();
-        Functions.NestedFact factNested = new  Functions.NestedFact();
+       Functions.NestedFact factNested = new  Functions.NestedFact();
        
        System.out.println("QuadratLamda");
-       f.applyAndPrint(0,10, f.quadrat);
+       f.applyAndPrint(1,10, f.quadrat);
        System.out.println("QuadratAnonym");
-       f.applyAndPrint(0,10, quadratAnonym);
+       f.applyAndPrint(1,10, quadratAnonym);
        System.out.println("FakultaetLamda");
-       f.applyAndPrint(0,10, f.fact);
+       f.applyAndPrint(1,10, f.fact);
        System.out.println("FakultaetAnonym");
-       f.applyAndPrint(0,10, factAnonym);
+       f.applyAndPrint(1,10, factAnonym);
        System.out.println("FakultaetTopLevel");
-       f.applyAndPrint(0,10, factTopLevel);
+       f.applyAndPrint(1,10, factTopLevel);
        System.out.println("FakultaetStaticNested");
-       f.applyAndPrint(0,10,factNested);
+       f.applyAndPrint(1,10,factNested);
        System.out.println("PotenzLamda");
-       f.applyAndPrint(0,10, f.potenz);
+       f.applyAndPrint(1,10, f.potenz);
        System.out.println("PotenzAnonym");
-       f.applyAndPrint(0,10, potenzAnonym);
+       f.applyAndPrint(1,10, potenzAnonym);
        System.out.println("FibonacciLamda");
-       f.applyAndPrint(0,10, f.fib);
+       f.applyAndPrint(1,10, f.fib);
        System.out.println("FibonacciAnonym");
-       f.applyAndPrint(0,10, fibAnonym);
+       f.applyAndPrint(1,10, fibAnonym);
        
        //nur zum schnell testen
        Predicate<Integer> GreaterThan5 =  x -> x > 5;
@@ -120,10 +134,23 @@ public class Functions
            
            System.out.println(i + ": " + functionP.conditionateOutput(GreaterThan5).apply(i));
        }
+
+       // Aufgabe e
+       System.out.println("Quadrat von geraden Zahlen:");
+       f.applyAndPrint(1,10, f.quadratVonGeradeZahlen);
+
+       // Aufgabe f
+       System.out.println("Fakultaet von ungeraden Zahlen:");
+       f.applyAndPrint(1,10, f.factVonUngerade);
    }
     
 
-   public void applyAndPrint(int i, int j, MyFunction f){
+   public void applyAndPrint(int i, int j, MyFunction f) {
+
+        if (i < 1 || j < 1) {
+            throw new IllegalArgumentException("Nur naturiche Zahlen werden unterstuetzt");
+        }
+
         if(j < i){
             throw new IllegalArgumentException("Die zweite Zahl muss groesser oder gleich der ersten Zahl sein.");
         }
@@ -132,6 +159,4 @@ public class Functions
             System.out.println(k + ": " + f.apply(k) + "\n");
         }
    }
-    
-    
 }
