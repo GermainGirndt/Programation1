784b921f97ef15e00ac21d504acde18232bcaaef
Katharina Krier <katharina.krier@web.de>
2022-07-03 14:41:46 +0200

    6

---
+++ ueb21/Reader.java
@@ -0,0 +1,58 @@
+import java.util.List;
+import java.util.LinkedList;
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.function.Function;
+import java.util.function.Consumer;
+
+/**
+ * Beschreiben Sie hier die Klasse Reader.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Reader
+{
+     List<String> list;
+
+    /**
+     * Konstruktor für Objekte der Klasse Reader
+     */
+    public Reader()
+    {
+         list = new LinkedList<>(); 
+    }
+    
+    
+    public List<String> readTextFile(String file, String prefix, int minLength, int limit){
+        File datei = new File(file);
+        try (BufferedReader bufferedReader  = new BufferedReader(new FileReader(datei))){
+            String zeile = "";
+            
+            while ((zeile = bufferedReader.readLine()) != null) {
+                String[] zeilesplit = zeile.trim().split(" ");  
+                for(String wort : zeilesplit){
+                    if(!wort.trim().isEmpty()){
+                        list.add(wort.trim());    
+                    }
+                }
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+       
+        
+        List<String> l = new LinkedList<>();
+        
+        list.stream().filter(s->s.length()>minLength)
+        .filter(s -> s.startsWith(prefix))
+        .sorted()
+        .forEach(s -> {if(l.size()<limit){l.add(s);}});
+        
+        
+        
+        return l;
+}
+}
